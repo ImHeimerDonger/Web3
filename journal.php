7a3ee4dd99d87e63fce9624e34f509795970fc5a
ImHeimerDonger <tolik-yakun@yandex.ru>
2020-12-24 01:00:07 +0400

    first commit

---
+++ journal.php
@@ -0,0 +1,164 @@
+<?php
+require_once("head.php");
+?>
+
+<body class="background_scroll">
+    <?php
+require_once("nav.php");
+?>
+    <table class="table table-hover table-dark table-striped table-bordered table-responsive-lg">
+        <thead>
+            <tr>
+                <th scope="col">№ п/п</th>
+                <th scope="col">Дата розничной продажи</th>
+                <th scope="col">Артикул</th>
+                <th scope="col">Наименование продукции</th>
+                <th scope="col">Количество (штук)</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>05.11.2077</td>
+                <td>3</td>
+                <td>Ибупрофен</td>
+                <td>134</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>05.11.2077</td>
+                <td>5</td>
+                <td>Ибупрофен</td>
+                <td>2</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>06.11.2077</td>
+                <td>2</td>
+                <td>Валидол</td>
+                <td>480</td>
+            </tr>
+            <tr>
+                <th scope="row">4</th>
+                <td>07.11.2077</td>
+                <td>1</td>
+                <td>Анальгин</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <th scope="row">5</th>
+                <td>09.11.2077</td>
+                <td>4</td>
+                <td>Аекол</td>
+                <td>-1</td>
+            </tr>
+            <tr>
+                <th scope="row">6</th>
+                <td>14.11.2077</td>
+                <td>6</td>
+                <td>Диазолин</td>
+                <td>8</td>
+            </tr>
+            <tr>
+                <th scope="row">7</th>
+                <td>19.11.2077</td>
+                <td>8</td>
+                <td>Римантадин</td>
+                <td>54</td>
+            </tr>
+            <tr>
+                <th scope="row">8</th>
+                <td>19.11.2077</td>
+                <td>3</td>
+                <td>Ибупрофен</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <th scope="row">9</th>
+                <td>20.11.2077</td>
+                <td>8</td>
+                <td>Римантадин</td>
+                <td>987</td>
+            </tr>
+            <tr>
+                <th scope="row">10</th>
+                <td>23.11.2077</td>
+                <td>9</td>
+                <td>Аскорутин</td>
+                <td>321</td>
+            </tr>
+            <tr>
+                <th scope="row">11</th>
+                <td>23.11.2077</td>
+                <td>7</td>
+                <td>Метронидазол</td>
+                <td>76</td>
+            </tr>
+            <tr>
+                <th scope="row">12</th>
+                <td>24.11.2077</td>
+                <td>9</td>
+                <td>Аскорутин</td>
+                <td>65</td>
+            </tr>
+            <tr>
+                <th scope="row">13</th>
+                <td>25.11.2077</td>
+                <td>4</td>
+                <td>Аекол</td>
+                <td>35</td>
+            </tr>
+            <tr>
+                <th scope="row">14</th>
+                <td>26.11.2077</td>
+                <td>1</td>
+                <td>Анальгин</td>
+                <td>2</td>
+            </tr>
+            <tr>
+                <th scope="row">15</th>
+                <td>01.12.2077</td>
+                <td>6</td>
+                <td>Диазолин</td>
+                <td>13</td>
+            </tr>
+            <tr>
+                <th scope="row">16</th>
+                <td>02.12.2077</td>
+                <td>8</td>
+                <td>Римантадин</td>
+                <td>75</td>
+            </tr>
+            <tr>
+                <th scope="row">17</th>
+                <td>05.12.2077</td>
+                <td>5</td>
+                <td>Карбамазепин</td>
+                <td>43</td>
+            </tr>
+            <tr>
+                <th scope="row">18</th>
+                <td>08.12.2077</td>
+                <td>7</td>
+                <td>Метронидазол</td>
+                <td>11</td>
+            </tr>
+            <tr>
+                <th scope="row">19</th>
+                <td>06.08.988</td>
+                <td>2</td>
+                <td>Валидол</td>
+                <td>339</td>
+            </tr>
+            <tr>
+                <th scope="row">20</th>
+                <td>06.03.988</td>
+                <td>1</td>
+                <td>Анальгин</td>
+                <td>339</td>
+            </tr>
+        </tbody>
+    </table>
+    <?php
+require_once("footer.php");
+?>
